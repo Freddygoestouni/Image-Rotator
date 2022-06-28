df985b0c858112a082ef1060f1c59b5ae9c728e1
Nicholas Williams <43884023+Freddygoestouni@users.noreply.github.com>
2022-06-28 11:37:42 +0100

    Basic Updated interface
    Added initial basic updated interface and integrated additional image features with the rotation functionality.
    
    Interface is not refined nor complete.

--- interface.py
+++ interface.py
@@ -11,6 +11,32 @@ import rotator
 import pkgutil
 
 
+# Constants
+
+button_pressed = """
+QPushButton{
+	background-color: rgba(0, 119, 182, 200);
+	border: 0px solid;
+	border-radius: 10px;
+	padding:7px;
+}
+"""
+
+button_not_pressed = """
+QPushButton{
+	background-color: rgba(202, 240, 248, 200);
+	border: 0px solid;
+	border-radius: 10px;
+	padding:7px;
+}
+QPushButton:hover{
+	background-color: rgba(0, 119, 182, 200);
+	border: 0px solid;
+	border-radius: 10px;
+	padding:7px;
+}
+"""
+
 # User Interface
 
 
@@ -20,16 +46,18 @@ class Interface(QWidget):
         # loadUi("user_interface.ui",self)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+
+
         # Files
         self.files = []
         self.destination = None
-        self.rotation = None
+        self.rotation = (None, None)    # Direction, Angle
         self.quality = None
+        self.alterations = ("Colour", ".jpg")   # Colour, Type
 
         # ------------------------------------------------------------------------------------------------------ #
         #       Stage One
 
-        # Choose File
         self.ui.button_choose_files.clicked.connect(self.choose_files)
 
         self.ui.label_no_files.setHidden(True)
@@ -38,38 +66,33 @@ class Interface(QWidget):
         # ------------------------------------------------------------------------------------------------------ #
         #       Stage Two
 
-        self.ui.ninety_clockwise.mouseReleaseEvent = self.option1
-        self.ui.ninety_counterclockwise.mouseReleaseEvent = self.option2
-        self.ui.onehundredeighty.mouseReleaseEvent = self.option3
-        self.ui.custom.mouseReleaseEvent = self.option4
+        self.ui.group_two.setHidden(True)
 
-        self.ui.angle.valueChanged.connect(self.customChange)
-        self.ui.clockwise.toggled.connect(self.customChange)
-        self.ui.counter_clockwise.toggled.connect(self.customChange)
+        self.ui.button_clockwise.clicked.connect(self.rotation_clockwise)
+        self.ui.button_counter_clockwise.clicked.connect(self.rotation_counter_clockwise)
 
-        self.ui.step_two.setHidden(True)
-        self.ui.clockwise.setHidden(True)
-        self.ui.counter_clockwise.setHidden(True)
+        self.ui.button_ninety.setHidden(True)
+        self.ui.button_one_eighty.setHidden(True)
+        self.ui.button_custom_rotation.setHidden(True)
         self.ui.angle.setHidden(True)
 
-        image1 = resource_path("clockwise.jpg")
-        image2 = resource_path("counter-clockwise.jpg")
-        image3 = resource_path("half-rotation.jpg")
+        self.ui.button_ninety.clicked.connect(self.rotation_ninety)
+        self.ui.button_one_eighty.clicked.connect(self.rotation_one_eighty)
+        self.ui.button_custom_rotation.clicked.connect(self.rotation_custom)
 
-        self.ui.label_ninety_clockwise_arrow.setPixmap(QtGui.QPixmap(image1))
-        self.ui.label_ninety_counterclockwise_arrow.setPixmap(QtGui.QPixmap(image2))
-        self.ui.label_onehundredeighty_arrow.setPixmap(QtGui.QPixmap(image3))
+        self.ui.angle.valueChanged.connect(self.rotation_custom_change)
+
 
         # ------------------------------------------------------------------------------------------------------ #
         #       Stage Three
 
         # Choose Quality
-        self.ui.button_same.clicked.connect(self.same_quality)
-        self.ui.button_different.clicked.connect(self.different_quality)
+        self.ui.button_same_quality.clicked.connect(self.quality_same)
+        self.ui.button_different_quality.clicked.connect(self.quality_different)
 
-        self.ui.percentage_quality.valueChanged.connect(self.qualityChange)
+        self.ui.percentage_quality.valueChanged.connect(self.quality_custom_change)
 
-        self.ui.step_three.setHidden(True)
+        self.ui.group_three.setHidden(True)
         self.ui.percentage_quality.setHidden(True)
         self.ui.label_percentage.setHidden(True)
 
@@ -77,24 +100,37 @@ class Interface(QWidget):
         # ------------------------------------------------------------------------------------------------------ #
         #       Stage Four
 
-        self.ui.button_choose_destination.clicked.connect(self.choose_destination)
+        self.ui.choose_colour.addItems(["Colour", "Black/White"])
+        self.ui.choose_file_type.addItems(["Same as Original", ".jpg", ".png"])
 
-        self.ui.label_destination.setHidden(True)
-        self.ui.step_four.setHidden(True)
+        self.ui.choose_colour.activated.connect(self.alterations_colour)
+        self.ui.choose_file_type.activated.connect(self.alterations_type)
+
+        self.ui.group_four.setHidden(True)
+
+
+        # ------------------------------------------------------------------------------------------------------ #
+        #       Stage Five
+
+        self.ui.button_choose_save.clicked.connect(self.choose_destination)
+
+        self.ui.group_five.setHidden(True)
+
 
 
         # ------------------------------------------------------------------------------------------------------ #
         #       Business Logic
-
-        # Start Rotation
-        self.ui.button_start.clicked.connect(self.start_rotations)
-
+    #
+    #     # Start Rotation
+    #     self.ui.button_start.clicked.connect(self.start_rotations)
+    #
         self.ui.button_start.setHidden(True)
-        self.ui.progressBar.setHidden(True)
-        self.ui.label_error_success.setHidden(True)
-
-
-
+        self.ui.button_start.clicked.connect(self.start)
+        # self.ui.progressBar.setHidden(True)
+        # self.ui.label_error_success.setHidden(True)
+    #
+    #
+    #
     # Method to handle the selection of the image files
     def choose_files(self):
         # Get File Names
@@ -107,123 +143,138 @@ class Interface(QWidget):
         self.ui.label_no_files.setHidden(False)
 
         if len(self.files) > 0:
-            self.ui.step_two.setHidden(False)
+            self.ui.group_two.setHidden(False)
 
 
     # --------------------------------------------------------------------------------------------------------- #
     #       Set Rotations
 
+    def rotation_clockwise(self):
+        self.rotation = ("Clockwise", self.rotation[1])
+        self.ui.button_ninety.setHidden(False)
+        self.ui.button_one_eighty.setHidden(False)
+        self.ui.button_custom_rotation.setHidden(False)
+        self.ui.button_clockwise.setStyleSheet(button_pressed)
+        self.ui.button_counter_clockwise.setStyleSheet(button_not_pressed)
 
-    def option1(self, irrelevant):
-        self.ui.ninety_clockwise.setStyleSheet("QWidget #ninety_clockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(85, 255, 255); }")
-        self.ui.ninety_counterclockwise.setStyleSheet("QWidget #ninety_counterclockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.onehundredeighty.setStyleSheet("QWidget #onehundredeighty { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.custom.setStyleSheet("QWidget #custom { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
+    def rotation_counter_clockwise(self):
+        self.rotation = ("Counter Clockwise", self.rotation[1])
+        self.ui.button_ninety.setHidden(False)
+        self.ui.button_one_eighty.setHidden(False)
+        self.ui.button_custom_rotation.setHidden(False)
+        self.ui.button_counter_clockwise.setStyleSheet(button_pressed)
+        self.ui.button_clockwise.setStyleSheet(button_not_pressed)
 
-        self.ui.clockwise.setHidden(True)
-        self.ui.counter_clockwise.setHidden(True)
+    def rotation_ninety(self):
+        self.rotation = (self.rotation[0], 90)
         self.ui.angle.setHidden(True)
+        self.ui.button_ninety.setStyleSheet(button_pressed)
+        self.ui.button_one_eighty.setStyleSheet(button_not_pressed)
+        self.ui.button_custom_rotation.setStyleSheet(button_not_pressed)
 
-        self.rotation = 90
-        self.ui.step_three.setHidden(False)
+        self.ui.group_three.setHidden(False)
 
-    def option2(self, irrelevant):
-        self.ui.ninety_clockwise.setStyleSheet("QWidget #ninety_clockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.ninety_counterclockwise.setStyleSheet("QWidget #ninety_counterclockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(85, 255, 255); }")
-        self.ui.onehundredeighty.setStyleSheet("QWidget #onehundredeighty { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.custom.setStyleSheet("QWidget #custom { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-
-        self.ui.clockwise.setHidden(True)
-        self.ui.counter_clockwise.setHidden(True)
+    def rotation_one_eighty(self):
+        self.rotation = (self.rotation[0], 180)
         self.ui.angle.setHidden(True)
+        self.ui.button_ninety.setStyleSheet(button_not_pressed)
+        self.ui.button_one_eighty.setStyleSheet(button_pressed)
+        self.ui.button_custom_rotation.setStyleSheet(button_not_pressed)
 
-        self.rotation = 270
-        self.ui.step_three.setHidden(False)
+        self.ui.group_three.setHidden(False)
 
-    def option3(self, irrelevant):
-        self.ui.ninety_clockwise.setStyleSheet("QWidget #ninety_clockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.ninety_counterclockwise.setStyleSheet("QWidget #ninety_counterclockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.onehundredeighty.setStyleSheet("QWidget #onehundredeighty { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(85, 255, 255); }")
-        self.ui.custom.setStyleSheet("QWidget #custom { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-
-        self.ui.clockwise.setHidden(True)
-        self.ui.counter_clockwise.setHidden(True)
-        self.ui.angle.setHidden(True)
-
-        self.rotation = 180
-        self.ui.step_three.setHidden(False)
-
-    def option4(self, irrelevant):
-        self.ui.ninety_clockwise.setStyleSheet("QWidget #ninety_clockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.ninety_counterclockwise.setStyleSheet("QWidget #ninety_counterclockwise { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.onehundredeighty.setStyleSheet("QWidget #onehundredeighty { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(0, 0, 0); }")
-        self.ui.custom.setStyleSheet("QWidget #custom { border-radius : 7px; border-width : 1px; border-style : solid; border-color: rgb(85, 255, 255); }")
-
-        self.ui.clockwise.setHidden(False)
-        self.ui.counter_clockwise.setHidden(False)
+    def rotation_custom(self):
         self.ui.angle.setHidden(False)
+        self.ui.button_ninety.setStyleSheet(button_not_pressed)
+        self.ui.button_one_eighty.setStyleSheet(button_not_pressed)
+        self.ui.button_custom_rotation.setStyleSheet(button_pressed)
 
-        if self.ui.clockwise.isChecked():
-            self.rotation = int(self.ui.angle.value())
-        else:
-            self.rotation = 360 - int(self.ui.angle.value())
-        self.ui.step_three.setHidden(False)
+        self.ui.group_three.setHidden(False)
 
-    def customChange(self):
-        if self.ui.clockwise.isChecked():
-            self.rotation = int(self.ui.angle.value())
-        else:
-            self.rotation = 360 - int(self.ui.angle.value())
+    def rotation_custom_change(self):
+        self.rotation = (self.rotation[0], int(self.ui.angle.value()))
 
+    # --------------------------------------------------------------------------------------------------------- #
+    #       Set Quality
 
-    # Method to handle the selection of the destination folder
+    def quality_same(self):
+        self.quality = 100
+        self.ui.button_same_quality.setStyleSheet(button_pressed)
+        self.ui.button_different_quality.setStyleSheet(button_not_pressed)
+        self.ui.percentage_quality.setHidden(True)
+        self.ui.label_percentage.setHidden(True)
+
+        self.ui.group_four.setHidden(False)
+        self.ui.group_five.setHidden(False)
+
+    def quality_different(self):
+        self.quality = int(self.ui.percentage_quality.value())
+        self.ui.button_same_quality.setStyleSheet(button_not_pressed)
+        self.ui.button_different_quality.setStyleSheet(button_pressed)
+        self.ui.percentage_quality.setHidden(False)
+        self.ui.label_percentage.setHidden(False)
+
+        self.ui.group_four.setHidden(False)
+        self.ui.group_five.setHidden(False)
+
+    def quality_custom_change(self):
+        self.quality = int(self.ui.percentage_quality.value())
+
+    # --------------------------------------------------------------------------------------------------------- #
+    #       Set Alterations
+
+    def alterations_type(self):
+        self.alterations = (self.alterations[0], self.ui.choose_file_type.currentText())
+
+    def alterations_colour(self):
+        self.alterations = (self.ui.choose_colour.currentText(), self.alterations[1])
+
+    # --------------------------------------------------------------------------------------------------------- #
+    #       Set Destination
+
     def choose_destination(self):
         # Get Destination
         destination_directory = QFileDialog.getExistingDirectory(self, 'Choose Save Directory')
 
         self.destination = destination_directory
 
-        self.ui.label_destination.setText(self.destination)
-        self.ui.label_destination.setHidden(False)
-
         if destination_directory is not None:
             self.ui.button_start.setHidden(False)
 
 
-    # Method for same quality
-    def same_quality(self):
-        self.quality = 100
-        self.ui.percentage_quality.setHidden(True)
-        self.ui.label_percentage.setHidden(True)
-        self.ui.step_four.setHidden(False)
-
-    # Method for different quality
-    def different_quality(self):
-        self.quality = int(self.ui.percentage_quality.value())
-        self.ui.percentage_quality.setHidden(False)
-        self.ui.label_percentage.setHidden(False)
-        self.ui.step_four.setHidden(False)
-
-    # Method for if the quality selection is to be changed
-    def qualityChange(self):
-        self.quality = int(self.ui.percentage_quality.value())
 
     # Method to start the business logic
-    def start_rotations(self):
-        self.ui.label_error_success.setHidden(True)
-        self.ui.progressBar.setHidden(False)
-        self.ui.progressBar.setValue(0)
+    def start(self):
+        print(self.files)
+        print(self.rotation)
+        print(self.quality)
+        print(self.alterations)
+        print(self.destination)
 
-        success = rotator.start(self.files, self.rotation, self.quality, self.destination, self.ui.progressBar)
+        success = rotator.start(filenames=self.files,
+                                             destination=self.destination,
+                                             angle=self.rotation[1],
+                                             direction=self.rotation[0],
+                                             quality=self.quality,
+                                             colour=self.alterations[0],
+                                             file_extension=self.alterations[1])
 
-        if success:
-            self.ui.label_error_success.setText("Success!")
-            self.ui.label_error_success.setStyleSheet("color: rgb(0, 170, 0);")
-        else:
-            self.ui.label_error_success.setText("Something went wrong!")
-            self.ui.label_error_success.setStyleSheet("color: rgb(255, 0, 0);")
+        print(success)
 
-        self.ui.label_error_success.setHidden(False)
+        # self.ui.label_error_success.setHidden(True)
+        # self.ui.progressBar.setHidden(False)
+        # self.ui.progressBar.setValue(0)
+
+        # success = rotator.start(self.files, self.rotation, self.quality, self.destination, self.ui.progressBar)
+
+        # if success:
+        #     self.ui.label_error_success.setText("Success!")
+        #     self.ui.label_error_success.setStyleSheet("color: rgb(0, 170, 0);")
+        # else:
+        #     self.ui.label_error_success.setText("Something went wrong!")
+        #     self.ui.label_error_success.setStyleSheet("color: rgb(255, 0, 0);")
+        #
+        # self.ui.label_error_success.setHidden(False)
 
 
 
@@ -232,5 +283,5 @@ class Interface(QWidget):
 def display(widget):
     screen = Interface(widget)
     widget.addWidget(screen)
-    widget.setFixedHeight(460)
-    widget.setFixedWidth(500)
+    widget.setFixedHeight(550)
+    widget.setFixedWidth(800)
--- rotator.py
+++ rotator.py
@@ -5,17 +5,19 @@ from PIL import Image
 import os, sys
 
 
-def start(filenames, angle, quality, destination, progressBar):
+def start(filenames, destination, angle, direction, quality, colour, file_extension, progressBar=None):
     try:
         for filename in filenames:
             image = getImage(filename)
 
-            rotated = rotate(image, angle)
+            image = rotate(image, angle, direction)
 
-            saveImage(destination, filename, rotated, quality)
+            image = colour_convert(image, colour)
 
-            progressBar.setValue(int(filenames.index(filename)+1.0*100.0/len(filenames)))
-        progressBar.setValue(100)
+            saveImage(destination, filename, image, quality, file_extension)
+
+            #progressBar.setValue(int(filenames.index(filename)+1.0*100.0/len(filenames)))
+        #progressBar.setValue(100)
         return True
     except:
         return False
@@ -23,11 +25,21 @@ def start(filenames, angle, quality, destination, progressBar):
 def getImage(filename):
     return Image.open(filename).convert('RGBA')
 
+def rotate(image, angle, direction):
+    if direction == "Clockwise":
+        angle = 360-angle
+    return image.rotate(angle, expand=True)
 
-def rotate(image, angle):
-    return image.rotate(360-angle, expand=True)
+def colour_convert(image, colour):
+    if colour == "Colour":
+        return image.convert('RGB')
+    elif colour == "Black/White":
+        return image.convert('1')
 
-def saveImage(destination, filename, image, quality):
+def saveImage(destination, filename, image, quality, extension):
     filename = filename[filename.rindex("/"):]
-    image = image.convert('RGB')
+
+    if extension != "Same as original":
+        filename = filename[:filename.rindex(".")] + extension
+
     image.save(destination+filename, quality=quality, optimize=True)
